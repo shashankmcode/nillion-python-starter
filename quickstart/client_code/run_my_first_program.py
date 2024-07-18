d3f9e7a4af86b615167c85c26d4d6437ac3cf0e6
shashankmcode <shashankhs5242@gmail.com>
2024-07-18 20:04:39 +0000

    my new nada program

--- quickstart/client_code/run_my_first_program.py
+++ quickstart/client_code/run_my_first_program.py
@@ -1,10 +1,38 @@
-from nada_dsl import *
+def perform_operation(op, num1, num2):
+    if op == '1':
+        return num1 + num2
+    elif op == '2':
+        return num1 - num2
+    elif op == '3':
+        return num1 * num2
+    elif op == '4':
+        if num2 != 0:
+            return num1 / num2
+        else:
+            return "Error: Division by zero is not allowed."
+    else:
+        return "Invalid operation selected."
 
-def nada_main():
-    party1 = Party(name="Party1")
-    my_int1 = SecretInteger(Input(name="my_int1", party=party1))
-    my_int2 = SecretInteger(Input(name="my_int2", party=party1))
+def math_calculator():
+    print("Select operation:")
+    print("1. Addition")
+    print("2. Subtraction")
+    print("3. Multiplication")
+    print("4. Division")
 
-    new_int = my_int1 * my_int2
+    # Get user input for operation
+    operation = input("Enter the number of the operation you want to perform: ")
 
-    return [Output(new_int, "my_output", party=party1)]
+    # Get user input for numbers
+    try:
+        number1 = float(input("Enter the first number: "))
+        number2 = float(input("Enter the second number: "))
+    except ValueError:
+        print("Invalid input. Please enter valid numbers.")
+        return
+
+    # Perform the selected operation
+    result = perform_operation(operation, number1, number2)
+    print(f"The result is: {result}")
+
+math_calculator()
